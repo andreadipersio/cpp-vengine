32bf85cf9b2b41f714d2cfae3598900a458a02db
Andrea Di Persio <git@andreadipersio.com>
2023-01-08 00:30:42 +0100

    Add project files.

---
+++ GameClock.cpp
@@ -0,0 +1,43 @@
+#include <chrono>
+
+using namespace std::chrono;
+
+class GameClock {
+public:
+	const unsigned short int& fps;
+
+	GameClock()
+		: frameDelta(0)
+		, fps(fps_)
+		, fps_(0)
+	{}
+
+	void atFrameStart() {
+		frameStart = steady_clock::now();
+	}
+
+	void atFrameEnd() {
+		frameEnd = steady_clock::now();
+		frameDelta = CalculateFrameDelta();
+		fps_ = CalculateFps();
+	}
+
+private:
+	time_point<steady_clock> frameStart;
+	time_point<steady_clock> frameEnd;
+	nanoseconds frameDelta;
+
+	unsigned short int fps_;
+
+	nanoseconds CalculateFrameDelta() {
+		return frameEnd - frameStart;
+	}
+
+	unsigned short int CalculateFps() {
+		if (frameDelta > 0ns) {
+			return (unsigned short int) (1s / frameDelta);
+		} else {
+			return 0;
+		}
+	}
+};
---
+++ Traum.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <format>
+
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+
+#include "GameClock.cpp"
+
+using namespace std;
+
+int main()
+{
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS | SDL_INIT_TIMER) != 0) {
+		cout << format("SDL_Init Error: {}", SDL_GetError()) << std::endl;
+	}
+
+	GameClock gameClock;
+
+	SDL_Window* win = SDL_CreateWindow(
+		"Game Engine",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		800, 600, 0);
+
+	bool run = true;
+
+	while (run) {
+		SDL_Event event;
+
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
+			case SDL_QUIT: 
+				run = false;
+				break;
+			case SDL_KEYDOWN:
+				SDL_Event quitEvent = { SDL_QUIT };
+				cout << format("FPS: {}", gameClock.fps) << std::endl;
+
+				switch (event.key.keysym.scancode) {
+				case SDL_SCANCODE_ESCAPE:
+					SDL_PushEvent(&quitEvent);
+				}
+				break;
+			}
+		}
+
+		gameClock.atFrameStart();
+		SDL_Delay(16);
+		gameClock.atFrameEnd();
+	}
+
+	SDL_DestroyWindow(win);
+	SDL_Quit();
+
+	return 0;
+}
