8db5173ca2cdd64d1935986c2046b02adc574026
Andrea Di Persio <git@andreadipersio.com>
2023-01-08 01:12:23 +0100

    Initial commit

--- GameClock.cpp
+++ GameClock.cpp
@@ -4,40 +4,31 @@ using namespace std::chrono;
 
 class GameClock {
 public:
-	const unsigned short int& fps;
+  const unsigned short int& fps;
 
-	GameClock()
-		: frameDelta(0)
-		, fps(fps_)
-		, fps_(0)
-	{}
+  GameClock() : frameDelta(0), fps(fps_), fps_(0) {}
 
-	void atFrameStart() {
-		frameStart = steady_clock::now();
-	}
+  void atFrameStart() { frameStart = steady_clock::now(); }
 
-	void atFrameEnd() {
-		frameEnd = steady_clock::now();
-		frameDelta = CalculateFrameDelta();
-		fps_ = CalculateFps();
-	}
+  void atFrameEnd() {
+    frameEnd = steady_clock::now();
+    frameDelta = CalculateFrameDelta();
+    fps_ = CalculateFps();
+  }
 
 private:
-	time_point<steady_clock> frameStart;
-	time_point<steady_clock> frameEnd;
-	nanoseconds frameDelta;
+  time_point<steady_clock> frameStart, frameEnd;
+  nanoseconds frameDelta;
 
-	unsigned short int fps_;
+  unsigned short int fps_;
 
-	nanoseconds CalculateFrameDelta() {
-		return frameEnd - frameStart;
-	}
+  nanoseconds CalculateFrameDelta() { return frameEnd - frameStart; }
 
-	unsigned short int CalculateFps() {
-		if (frameDelta > 0ns) {
-			return (unsigned short int) (1s / frameDelta);
-		} else {
-			return 0;
-		}
-	}
+  unsigned short int CalculateFps() {
+    if (frameDelta > 0ns) {
+      return (unsigned short int)(1s / frameDelta);
+    } else {
+      return 0;
+    }
+  }
 };
--- Traum.cpp
+++ Traum.cpp
@@ -1,5 +1,6 @@
-﻿#include <iostream>
-#include <format>
+﻿#include <format>
+#include <map>
+#include <iostream>
 
 #define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
@@ -8,49 +9,65 @@
 
 using namespace std;
 
-int main()
-{
-	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS | SDL_INIT_TIMER) != 0) {
-		cout << format("SDL_Init Error: {}", SDL_GetError()) << std::endl;
-	}
+int main() {
+  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS | SDL_INIT_GAMECONTROLLER) != 0) {
+    cout << format("SDL_Init Error: {}", SDL_GetError()) << std::endl;
+  }
 
-	GameClock gameClock;
+  GameClock gameClock;
 
-	SDL_Window* win = SDL_CreateWindow(
-		"Game Engine",
-		SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED,
-		800, 600, 0);
+  SDL_Window* win = SDL_CreateWindow("Game Engine", SDL_WINDOWPOS_CENTERED,
+                                     SDL_WINDOWPOS_CENTERED, 800, 600, 0);
 
-	bool run = true;
 
-	while (run) {
-		SDL_Event event;
+  map<int32_t, SDL_GameController*> controllers;
 
-		while (SDL_PollEvent(&event)) {
-			switch (event.type) {
-			case SDL_QUIT: 
-				run = false;
-				break;
-			case SDL_KEYDOWN:
-				SDL_Event quitEvent = { SDL_QUIT };
-				cout << format("FPS: {}", gameClock.fps) << std::endl;
+  bool run = true; 
 
-				switch (event.key.keysym.scancode) {
-				case SDL_SCANCODE_ESCAPE:
-					SDL_PushEvent(&quitEvent);
-				}
-				break;
-			}
-		}
+  while (run) {
+    SDL_Event event;
 
-		gameClock.atFrameStart();
-		SDL_Delay(16);
-		gameClock.atFrameEnd();
-	}
+    while (SDL_PollEvent(&event)) {
+      switch (event.type) {
+      case SDL_QUIT:
+        run = false;
+        break;
+      case SDL_CONTROLLERDEVICEADDED: {
+        auto controllerId = event.cdevice.which;
+        cout << format("Controller with id '{}' has been detected", controllerId) << std::endl;
+        SDL_GameControllerOpen(controllerId);
+        auto controller = SDL_GameControllerFromInstanceID(controllerId);
+        if (controller) {
+          controllers[(int32_t)controllerId] = controller;
+        } else {
+          cout << format("Controller with id '{}' could not be opened: {}",
+                         controllerId, SDL_GetError())
+               << std::endl;
+        }
+        break;
+      } 
+      case SDL_CONTROLLERBUTTONDOWN:
+        cout << format("FPS: {}", gameClock.fps) << std::endl;
+        break;
+      case SDL_KEYDOWN:
+        SDL_Event quitEvent = {SDL_QUIT};
+        cout << format("FPS: {}", gameClock.fps) << std::endl;
 
-	SDL_DestroyWindow(win);
-	SDL_Quit();
+        switch (event.key.keysym.scancode) {
+        case SDL_SCANCODE_ESCAPE:
+          SDL_PushEvent(&quitEvent);
+        }
+        break;
+      }
+    }
 
-	return 0;
+    gameClock.atFrameStart();
+    SDL_Delay(16);
+    gameClock.atFrameEnd();
+  }
+
+  SDL_DestroyWindow(win);
+  SDL_Quit();
+
+  return 0;
 }
