67c3655f068e5a36768bc556f07a12377615bdce
Andrea Di Persio <git@andreadipersio.com>
2023-02-06 08:46:53 +0100

    Draw textures using blitsurface  (1 texture per widget)

--- menu_render.cpp
+++ menu_render.cpp
@@ -4,6 +4,7 @@ Menu_render::Menu_render(menu::Menu_manager& menu_manager)
 	: menu_manager_(menu_manager) {}
 
 void Menu_render::operator()(Render_context& sdl_ctx, Render_offset& offset) {
+
 	for (auto& menu_entry : menu_manager_) {
 		Menu_entry_render menu_entry_render{ menu_entry };
 		menu_entry_render(sdl_ctx, offset);
--- menu_widget_render.cpp
+++ menu_widget_render.cpp
@@ -4,50 +4,71 @@ Menu_widget_choice_render::Menu_widget_choice_render(menu::Choice_widget& widget
 	: widget_(widget) {};
 
 void Menu_widget_choice_render::operator()(Render_context& sdl_ctx, Render_offset offset) {
-		auto current_choice = widget_.current_choice();
-		auto font = sdl_ctx.fonts[sdl::MENU_MEDIUM_FONT].get();
-		SDL_Color active_color = { 255, 255, 255 };
-		SDL_Color inactive_color = { 127, 133, 135 };
+	auto current_choice = widget_.current_choice();
+	auto font = sdl_ctx.fonts[sdl::MENU_MEDIUM_FONT].get();
+	SDL_Color active_color = { 255, 255, 255 };
+	SDL_Color inactive_color = { 127, 133, 135 };
 
-		std::vector<std::tuple<string, SDL_Color>> text_blocks;
+	std::vector<std::tuple<string, SDL_Color>> text_blocks;
 
-		{
-			SDL_Color color;
+	{
+		SDL_Color color;
 
-			if (widget_.is_first())
-				color = inactive_color;
-			else
-				color = active_color;
+		if (widget_.is_first())
+			color = inactive_color;
+		else
+			color = active_color;
 
-			text_blocks.push_back({ "<", color });
+		text_blocks.push_back({ "<", color });
+	}
+
+	text_blocks.push_back({ current_choice->label(), active_color });
+
+	{
+		SDL_Color color;
+
+		if (widget_.is_last())
+			color = inactive_color;
+		else
+			color = active_color;
+
+		text_blocks.push_back({ ">", color });
+	}
+
+	uint16_t width = 0;
+	uint16_t height = 0;
+
+	std::vector<Surface_ptr> surfaces;
+
+	for (auto [text, color] : text_blocks) {
+		Surface_ptr surface{ TTF_RenderText_Solid(font, text.c_str(), color) };
+
+		width += surface->w + 10;
+
+		if (surface->h > height) {
+			height = surface->h;
 		}
 
-		text_blocks.push_back({ current_choice->label(), active_color});
+		surfaces.emplace_back(std::move(surface));
+	}
 
-		{
-			SDL_Color color;
+	Surface_ptr surface{ SDL_CreateRGBSurface(0, width, height, 16, 0, 0, 0, 0) };
 
-			if (widget_.is_last())
-				color = inactive_color;
-			else
-				color = active_color;
+	uint16_t offset_x = 0;
 
-			text_blocks.push_back({ ">", color });
-		}
+	for (auto& s : surfaces) {
+		SDL_Rect dst = { offset_x, 0, s->w, s->h };
+		SDL_BlitSurface(s.get(), NULL, surface.get(), &dst);
+		offset_x += s->w + 10;
+	}
 
-		for (auto [text, color] : text_blocks) {
-				Surface_ptr surface { TTF_RenderText_Solid(font, text.c_str(), color) };
-				Texture_ptr texture{ SDL_CreateTextureFromSurface(sdl_ctx.r.get(), surface.get()) };
+	SDL_Rect dest = {
+		offset.x,
+		offset.y,
+		surface->w,
+		surface->h
+	};
 
-				SDL_Rect dest = {
-					offset.x,
-					offset.y,
-					surface->w,
-					surface->h
-				};
-
-				SDL_RenderCopy(sdl_ctx.r.get(), texture.get(), NULL, &dest);
-
-				offset.x += surface->w + 10;
-		}
+	Texture_ptr texture{ SDL_CreateTextureFromSurface(sdl_ctx.r.get(), surface.get()) };
+	SDL_RenderCopy(sdl_ctx.r.get(), texture.get(), NULL, &dest);
 }
